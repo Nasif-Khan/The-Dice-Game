4bc17bb2548616af1397395fcc0591a9fb211459
Nasif-Khan <95160536+Nasif-Khan@users.noreply.github.com>
2023-03-20 21:16:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var number1 = Math.floor(Math.random()*6)+1;
+var number2 = Math.floor(Math.random()*6)+1;
+
+var firstDice = "images/dice" + number1 + ".png";
+var secondDice = "images/dice" + number2 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",firstDice);
+document.querySelectorAll("img")[1].setAttribute("src",secondDice);
+
+if(number1 > number2){
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if(number2 > number1){
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
